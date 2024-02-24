a8c7765391362659b7f754403ad0840e0af30f38
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-24 11:48:45 +0200

    add prettier plugin for sorting tailwindcss classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-    return (
-        <div className="main-layout">
-            <div className="nav bg-slate-500">Nav</div>
-            <div className="aside bg-slate-600">Aside</div>
-            <div className="main bg-slate-700">Main</div>
-        </div>
-    )
+  return (
+    <div className="main-layout">
+      <div className="nav bg-slate-500">Nav</div>
+      <div className="aside bg-slate-600">Aside</div>
+      <div className="main bg-slate-700">Main</div>
+    </div>
+  )
 }
 
 export default App
