c2fe2bf9e02c0b9b284ff8412c3d83c0f876e645
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-24 11:44:19 +0200

    create main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-    return <h1>Hello Game Hub React</h1>
+    return (
+        <div className="main-layout">
+            <div className="nav bg-slate-500">Nav</div>
+            <div className="aside bg-slate-600">Aside</div>
+            <div className="main bg-slate-700">Main</div>
+        </div>
+    )
 }
 
 export default App
