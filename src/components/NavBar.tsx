c795ee96fbbf0b156224193639885fe681281173
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-24 17:44:07 +0200

    increase nav bar padding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
-    <nav className="flex items-center justify-between p-2">
-      <img className="h-20 w-20" src={logoUrl} alt="game hub logo" />
+    <nav className="flex items-center justify-between p-2.5">
+      <img className="h-16" src={logoUrl} alt="game hub logo" />
       <ColorModeSwitch />
     </nav>
   )
