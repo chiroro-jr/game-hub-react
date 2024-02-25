08e403333592c9bcc7af026cfc739c550cfeca0f
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-25 17:34:13 +0200

    add genres title

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -15,22 +15,25 @@ function GenresList({ onSelectGenre, selectedGenre }: Props) {
   if (isLoading) return <GenresListSkeleton />
 
   return (
-    <ul className="space-y-2.5">
-      {genres.map((genre) => (
-        <li key={genre.id} onClick={() => onSelectGenre(genre.id)}>
-          <a
-            href="#"
-            className={`${selectedGenre === genre.id && 'font-bold'} flex items-center gap-1.5 hover:underline`}
-          >
-            <img
-              className="h-8 w-8 rounded-md object-cover"
-              src={cropImage(genre.image_background)}
-            />
-            <span className="wrap">{genre.name}</span>
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div className="space-y-2.5">
+      <h2 className="text-lg font-bold">Genres</h2>
+      <ul className="space-y-2.5">
+        {genres.map((genre) => (
+          <li key={genre.id} onClick={() => onSelectGenre(genre.id)}>
+            <a
+              href="#"
+              className={`${selectedGenre === genre.id && 'font-bold'} flex items-center gap-1.5 hover:underline`}
+            >
+              <img
+                className="h-8 w-8 rounded-md object-cover"
+                src={cropImage(genre.image_background)}
+              />
+              <span className="wrap">{genre.name}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
