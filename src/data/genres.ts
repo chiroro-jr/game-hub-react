7d120e8680e8cafac83d6698343155b6f153b036
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-26 14:42:20 +0200

    create static data for genres, platforms, and sort orders

---
+++ src/data/genres.ts
@@ -0,0 +1,137 @@
+const genres = [
+  {
+    id: 4,
+    name: 'Action',
+    slug: 'action',
+    image_background:
+      'https://media.rawg.io/media/games/490/49016e06ae2103881ff6373248843069.jpg',
+  },
+  {
+    id: 51,
+    name: 'Indie',
+    slug: 'indie',
+    image_background:
+      'https://media.rawg.io/media/games/0be/0bea0a08a4d954337305391b778a7f37.jpg',
+  },
+  {
+    id: 3,
+    name: 'Adventure',
+    slug: 'adventure',
+    image_background:
+      'https://media.rawg.io/media/games/d5a/d5a24f9f71315427fa6e966fdd98dfa6.jpg',
+  },
+  {
+    id: 5,
+    name: 'RPG',
+    slug: 'role-playing-games-rpg',
+    image_background:
+      'https://media.rawg.io/media/games/d0f/d0f91fe1d92332147e5db74e207cfc7a.jpg',
+  },
+  {
+    id: 10,
+    name: 'Strategy',
+    slug: 'strategy',
+    image_background:
+      'https://media.rawg.io/media/games/0bd/0bd5646a3d8ee0ac3314bced91ea306d.jpg',
+  },
+  {
+    id: 2,
+    name: 'Shooter',
+    slug: 'shooter',
+    image_background:
+      'https://media.rawg.io/media/games/34b/34b1f1850a1c06fd971bc6ab3ac0ce0e.jpg',
+  },
+  {
+    id: 40,
+    name: 'Casual',
+    slug: 'casual',
+    image_background:
+      'https://media.rawg.io/media/games/a44/a444a7628bdb49b24d06a7672f805814.jpg',
+  },
+  {
+    id: 14,
+    name: 'Simulation',
+    slug: 'simulation',
+    image_background:
+      'https://media.rawg.io/media/games/48c/48cb04ca483be865e3a83119c94e6097.jpg',
+  },
+  {
+    id: 7,
+    name: 'Puzzle',
+    slug: 'puzzle',
+    image_background:
+      'https://media.rawg.io/media/screenshots/c97/c97b943741f5fbc936fe054d9d58851d.jpg',
+  },
+  {
+    id: 11,
+    name: 'Arcade',
+    slug: 'arcade',
+    image_background:
+      'https://media.rawg.io/media/games/363/36306deef81e7955a5d0f5c3b43fccee.jpg',
+  },
+  {
+    id: 83,
+    name: 'Platformer',
+    slug: 'platformer',
+    image_background:
+      'https://media.rawg.io/media/games/4cb/4cb855e8ef1578415a928e53c9f51867.png',
+  },
+  {
+    id: 1,
+    name: 'Racing',
+    slug: 'racing',
+    image_background:
+      'https://media.rawg.io/media/games/19a/19a512a8c1293c22894b7364e1405ec1.jpg',
+  },
+  {
+    id: 59,
+    name: 'Massively Multiplayer',
+    slug: 'massively-multiplayer',
+    image_background:
+      'https://media.rawg.io/media/games/11f/11fd681c312c14644ab360888dba3486.jpg',
+  },
+  {
+    id: 15,
+    name: 'Sports',
+    slug: 'sports',
+    image_background:
+      'https://media.rawg.io/media/games/be5/be51faf9bec778b4ea1b06e9b084792c.jpg',
+  },
+  {
+    id: 6,
+    name: 'Fighting',
+    slug: 'fighting',
+    image_background:
+      'https://media.rawg.io/media/screenshots/0df/0df4aff421976b2a84d840faefb04124.jpg',
+  },
+  {
+    id: 19,
+    name: 'Family',
+    slug: 'family',
+    image_background:
+      'https://media.rawg.io/media/games/694/6940fa3fbe0d836e4a272c468e65e480.jpg',
+  },
+  {
+    id: 28,
+    name: 'Board Games',
+    slug: 'board-games',
+    image_background:
+      'https://media.rawg.io/media/screenshots/8ff/8ffe8f19d2e764867c8ed625ddf4e368.jpg',
+  },
+  {
+    id: 34,
+    name: 'Educational',
+    slug: 'educational',
+    image_background:
+      'https://media.rawg.io/media/games/6d3/6d33014a4ed48a19c30a77ead5a0f62e.jpg',
+  },
+  {
+    id: 17,
+    name: 'Card',
+    slug: 'card',
+    image_background:
+      'https://media.rawg.io/media/screenshots/1be/1be2141edae05d4ba9858182b081e604.jpg',
+  },
+]
+
+export default genres
---
+++ src/data/orderings.ts
@@ -0,0 +1,10 @@
+const orderings = [
+  { value: 'relevance', label: 'Relevance' },
+  { value: 'name', label: 'Name' },
+  { value: '-released', label: 'Release date' },
+  { value: '-added', label: 'Date added' },
+  { value: '-metacritic', label: 'Average rating' },
+  { value: '-popularity', label: 'Popularity' },
+]
+
+export default orderings
