5c9e52248019274b725e1303f6aaa34a6f4c60fa
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-02-29 15:10:12 +0200

    create reusable http client

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -1,15 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export interface FetchDataResponse<T> {
   count: number
   results: T[]
 }
 
-const httpClient = axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: 'fc992f5179d14507bfc1bddc17a9c03c',
   },
 })
 
-export default httpClient
+class HttpClient<T> {
+  endpoint: string
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint
+  }
+
+  getAll = (requestConfig?: AxiosRequestConfig) =>
+    axiosInstance
+      .get<FetchDataResponse<T>>(this.endpoint, requestConfig)
+      .then((res) => res.data)
+}
+
+export default HttpClient
