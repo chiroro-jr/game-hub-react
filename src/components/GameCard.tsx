ad2e00e505d6b9b1a2ced2ceed7fb300b8ddaf9e
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-03-09 17:28:39 +0200

    use critic score component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,8 +2,8 @@ import { Game } from '@/entities/Game'
 import { Card, CardContent, CardTitle } from './ui/card'
 import PlatformIconsList from './PlatformIconsList'
 import { cropImage } from '@/services/image-url'
-import { Badge } from '@/components/ui/badge'
 import { Link } from 'react-router-dom'
+import CriticScore from './CriticScore'
 
 interface Props {
   game: Game
@@ -12,13 +12,6 @@ interface Props {
 // TODO: Use an AspectRatio component for the card image
 // TODO: Fix badges for games that don't have a rating
 function GameCard({ game }: Props) {
-  const badgeColor =
-    game.metacritic > 85
-      ? 'bg-green-600'
-      : game.metacritic > 70
-        ? 'bg-yellow-500'
-        : 'bg-gray-500'
-
   return (
     <Link to={`/games/${game.id}`}>
       <Card className="overflow-hidden rounded-xl border-none shadow-md transition-all duration-150 hover:-translate-y-2 hover:scale-[1.02] ">
@@ -27,12 +20,7 @@ function GameCard({ game }: Props) {
           <CardTitle className="text-2xl">{game.name}</CardTitle>
           <div className="flex items-center justify-between">
             <PlatformIconsList platforms={game.parent_platforms} />
-            <Badge
-              className={`${badgeColor} rounded-sm px-2 text-[14px]`}
-              variant="secondary"
-            >
-              {game.metacritic}
-            </Badge>
+            <CriticScore score={game.metacritic} />
           </div>
         </CardContent>
       </Card>
