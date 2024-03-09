177332e907dd45237189d4ed9999433cbb7e9a3f
Nyasha D. Chiroro <chirorodennis@gmail.com>
2024-03-09 17:31:36 +0200

    add link to home page in NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import logoUrl from '../assets/images/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
@@ -5,7 +6,9 @@ import SearchInput from './SearchInput'
 const NavBar = () => {
   return (
     <nav className="flex items-center gap-2 p-3.5 md:gap-4">
-      <img className="h-16" src={logoUrl} alt="game hub logo" />
+      <Link to="/">
+        <img className="w-16" src={logoUrl} alt="game hub logo" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </nav>
